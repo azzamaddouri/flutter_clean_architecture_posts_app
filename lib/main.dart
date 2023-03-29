9c5a7ac5d0826719fbfb48cbb26b73a6159b58db
azzamaddouri <maddouriazza@gmail.com>
2023-03-29 23:13:20 +0200

    Calling Blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture_posts_app/core/theme/app_theme.dart';
+import 'package:flutter_clean_architecture_posts_app/features/posts/domain/userscases/get_all_posts.dart';
+import 'package:flutter_clean_architecture_posts_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/posts/presentation/bloc/posts/posts_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,theme: appTheme, home: Scaffold(appBar: AppBar()));
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => PostsBloc(getAllPosts: GetAllPostsUseCase()),
+          ),
+          BlocProvider(
+            create: (context) => AddDeleteUpdatePostBloc(addPost: addPost, updatePost: updatePost, deletePost: deletePost),
+          ),
+        ],
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: appTheme,
+            home: Scaffold(appBar: AppBar())));
   }
 }
