211d4cd9dc2de99b151c4d79433b5cb88b1c3754
azzamaddouri <maddouriazza@gmail.com>
2023-04-01 23:38:19 +0200

    Still working on delete post

--- lib/features/posts/presentation/widgets/post_datail_page/post_detail_widget.dart
+++ lib/features/posts/presentation/widgets/post_datail_page/post_detail_widget.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture_posts_app/core/util/snackbar_message.dart';
+import 'package:flutter_clean_architecture_posts_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
 import 'package:flutter_clean_architecture_posts_app/features/posts/presentation/pages/post_add_update_page.dart';
+import 'package:flutter_clean_architecture_posts_app/features/posts/presentation/pages/posts_page.dart';
 
 import '../../../domain/entities/post_entity.dart';
 
@@ -41,11 +45,43 @@ class PageDetailWidget extends StatelessWidget {
                                 isUpdatePost: true, post: post)));
                   },
                   icon: Icon(Icons.edit),
-                  label: Text("Edit"))
+                  label: Text("Edit")),
+              ElevatedButton.icon(
+                onPressed: deleteDialog,
+                icon: Icon(Icons.delete_outlined),
+                label: Text("Delete"),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueAccent)),
+              )
             ],
           )
         ],
       ),
     );
+  }
+
+  void deleteDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return BlocConsumer<AddDeleteUpdatePostBloc,
+              AddDeleteUpdatePostState>(
+            listener: (context, state) {
+              if (state is MessageAddDeleteUpdatePostState) {
+                SnackBarMessage().showErrorSnackBar(
+                    message: state.message, context: context);
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (_) => PostsPage()),
+                    (route) => false);
+              } else if (state is ErrorAddDeleteUpdatePostState) {
+                Navigator.of(context).pop();
+              }
+            },
+            builder: (context, state) {
+              return Container();
+            },
+          );
+        });
   }
 }
